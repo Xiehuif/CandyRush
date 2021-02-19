537043be0ebb5abb2649c4b566e7ea717068570a
XHL <2682950883@qq.com>
2021-02-19 23:24:04 +0800

    -m添加奶糖第一关

---
+++ Assets/Scripts/FourthRound/DeadlyElectric.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadlyElectric : MonoBehaviour
+{
+    public bool status = true;
+
+    private SpriteRenderer m_spriteRenderer;
+
+    private void Start()
+    {
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+        ChangeColor();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!status)
+            return;
+        else if (collision.CompareTag("Player"))
+            collision.GetComponentInChildren<Death>().IsDeath();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            status = !status;
+            ChangeColor();
+        }
+    }
+
+    private void ChangeColor()
+    {
+        if (!status)
+            m_spriteRenderer.color = new Color(0.46f, 0.57f, 0.8f);
+        else
+            m_spriteRenderer.color = new Color(0, 0, 1f);
+    }
+}
---
+++ Assets/Scripts/FourthRound/HoldMoveTrack.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class HoldMoveTrack : MonoBehaviour
+{
+    public Transform origin;//初相
+    public Transform end;//反相
+    public float speed = 3f;//到位速度
+
+    private Vector3 m_originPos;//初相位置
+    private Vector3 m_endPos;//反相位置
+    private Vector3 m_dir;//方向
+    private readonly float m_backSpeed = 6f;//返回速度
+
+    private void Start()
+    {
+        m_originPos = origin.position;
+        m_endPos = end.position;
+        m_dir = (m_endPos - m_originPos).normalized;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (Vector3.Dot((transform.position - m_endPos), m_dir) > 0)
+                transform.position = m_endPos;
+            else
+                transform.position += speed * m_dir * Time.deltaTime;
+        }
+        else
+        {
+            if (Vector3.Dot((transform.position - m_originPos), m_dir) < 0)
+                transform.position = m_originPos;
+            else
+                transform.position -= speed * m_dir * Time.deltaTime;
+        }
+    }
+
+#if UNITY_EDITOR
+
+    protected void OnDrawGizmosSelected()
+    {
+        UnityEditor.Handles.color = Color.red;
+        UnityEditor.Handles.DrawLine(origin.position, end.position);
+    }
+
+#endif
+}
