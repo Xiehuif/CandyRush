90c5b5a92f95b94e9126f052b9397428d8cac29b
yanghanbo <3535493428@qq.com>
2021-03-05 21:20:20 +0800

    素材 拉糖实装

---
+++ Assets/Scripts/Item/Hold/RealPullSugar.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealPullSugar : MonoBehaviour
+{
+    public Transform[] itemsToRotate;
+   public float PullDuartion;
+   private GameObject m_player;
+   private bool m_BeginToPull = false;
+   private float m_wholetime = 0;
+   void Update()
+   {
+       if(m_BeginToPull)
+       {
+           if(Input.GetMouseButton(0))
+           {
+               m_wholetime += Time.unscaledDeltaTime;
+               if(m_wholetime < PullDuartion)
+               { 
+                   foreach(Transform t in itemsToRotate)
+                   {
+                        t.rotation *= Quaternion.Euler(0, 0, Time.unscaledDeltaTime * 180f);
+                   }
+                   m_player.transform.rotation *= Quaternion.Euler(0, 0, Time.unscaledDeltaTime * 180f);
+               }
+               else
+               {
+                   m_BeginToPull = false;
+                   TimeManager.Instance.Continue();
+                   m_player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+               }
+           }
+       }
+   }
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+        Debug.Log("Enter");
+       if (other.CompareTag("Player"))
+        {
+            TimeManager.Instance.Pause();
+            m_player = other.gameObject;
+            m_player.transform.position = this.transform.position;
+            m_player.transform.rotation = Quaternion.Euler(0,0,0);
+            m_player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+            m_BeginToPull = true;
+            Debug.Log("Enter");
+        }
+       
+   }
+}
---
+++ Assets/Scripts/Manager/AppearanceManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearanceManager : Singleton<AppearanceManager>
+{
+   Sprite[] appearance;
+   public int AppearanceCount 
+   {
+        get;
+        set;
+    }
+}
