3bba49c7ceca1ee945273418f9a7364c56598aed
yanghanbo <3535493428@qq.com>
2021-03-12 18:05:57 +0800

    新的第二关

---
+++ Assets/Scripts/SencondRound/Others/FreezeArea.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeArea : MonoBehaviour
+{
+    public float FreezeTime = 1f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+
+            collision.gameObject.transform.rotation =  Quaternion.identity;
+            Rigidbody2D rig = collision.gameObject.GetComponent<Rigidbody2D>();
+            rig.freezeRotation = true;
+            TimeManager.Instance.DelayDo(() => { rig.freezeRotation = false; }, FreezeTime);
+        }
+    }
+}
