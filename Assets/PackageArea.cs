4c8fd172fccc2f2943d816e4944facd32ff72648
Xiehuif <72752074+Xiehuif@users.noreply.github.com>
2021-03-13 23:35:59 +0800

    包装机关

---
+++ Assets/PackageArea.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PackageArea : MonoBehaviour
+{
+    public enum Quality
+    {
+        bad,
+        great,
+        best
+    }
+    public InPackage final;
+    private Quality selfQuality;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string name = this.gameObject.name;
+        if (name == "Bad") selfQuality = Quality.bad;
+        if (name == "Great") selfQuality = Quality.great;
+        if (name == "Best") selfQuality = Quality.best;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (selfQuality == Quality.bad) final.inActive = true;
+        if (final.locked || collision.tag != "Player") return;
+        else
+        {
+            Debug.Log(selfQuality);
+            final.checkQuality = selfQuality;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
