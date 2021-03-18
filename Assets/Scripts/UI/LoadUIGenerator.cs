107ec6c25341f24b0e8ff36dc8bfe9fd87b29708
XHL <2682950883@qq.com>
2021-03-18 17:08:10 +0800

    添加加载UI

---
+++ Assets/Scripts/UI/LoadUIGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadUIGenerator : MonoBehaviour
+{
+    private GameObject m_targetUI;
+
+    private void OnLevelWasLoaded(int level)
+    {
+        var f = (GameObject)Resources.Load("LoadingCanvas");
+        m_targetUI=Instantiate(f);
+        TimeManager.Instance.Pause();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            TimeManager.Instance.Continue();
+            Destroy(m_targetUI);
+            Destroy(this);
+        }
+    }
+}
