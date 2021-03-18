c9c924d8a7bea89824aab92a347105be3248732c
yanghanbo <3535493428@qq.com>
2021-03-18 18:57:06 +0800

    删除多余关卡 加上简单的资源加载

---
+++ Assets/Scripts/Manager/ResourceManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.Events;
+using UnityEngine.AddressableAssets;
+using UnityEngine;
+
+namespace Pattern
+{
+    public class ResourceManager : Singleton<ResourceManager>
+    {
+        public void LoadAssetAsyn<T>(string key, UnityAction<T> onCompleted)
+        {
+            var loader = Addressables.LoadAssetAsync<T>(key);
+            if (!loader.IsValid())
+            {
+                Debug.LogError($"Error: failed at loading {key}");
+                return;
+            }
+            loader.Completed += handle =>
+            {
+                var result = handle.Result;
+                onCompleted?.Invoke(result);
+            };
+        }
+
+        public void Unload<T>(T obj)
+        {
+            Addressables.Release(obj);
+        }
+    }
+}
