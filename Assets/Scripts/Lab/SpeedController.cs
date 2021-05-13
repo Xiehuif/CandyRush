3f6051c3bbce7f4e78bb134976d021699a8d5001
yanghanbo <3535493428@qq.com>
2021-05-13 15:29:40 +0800

    尝试改改MoveTrack

---
+++ Assets/Scripts/Lab/SpeedController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedController : MonoBehaviour
+{
+    public float Speed;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            collision.collider.GetComponent<LabMov>().ChangeSpeed(Speed);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            collision.collider.GetComponent<LabMov>().Recover();
+        }
+
+    }
+}
--- Assets/Scripts/LabMov.cs
+++ Assets/Scripts/LabMov.cs
@@ -8,11 +8,23 @@ public class LabMov : MonoBehaviour
     public Vector2 offset = Vector2.zero;
     private Rigidbody2D rigidbody2D;
     public float Speed;
+    private float m_RealSpeed;
+    public float Width;
     public bool HasLeave;
     private float LeaveTime;
+    public void ChangeSpeed(float speed)
+    {
+        if (speed < 0 || speed > 1e5) return;
+        m_RealSpeed = Speed;
+    }
+    public void Recover()
+    {
+        m_RealSpeed = Speed;
+    }
     private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        Recover();
     }
     private void Update()
     {
@@ -20,13 +32,15 @@ public class LabMov : MonoBehaviour
         {
             LeaveTime += Time.deltaTime;
         }
+
     }
     void FixedUpdate()
     {
+        float angleOffset = Mathf.Abs(Mathf.Sin(transform.rotation.z)) * Width;
         RaycastHit2D[] hits = new RaycastHit2D[3];
-        hits[0] =  Physics2D.Raycast((Vector2)transform.position + offset, -1 * transform.up,distance);
-        hits[2] = Physics2D.Raycast((Vector2)transform.position - offset, -1 * transform.up, distance);
-        hits[1] = Physics2D.Raycast((Vector2)transform.position, -1 * transform.up, distance);
+        hits[0] =  Physics2D.Raycast((Vector2)transform.position + offset, -1 * transform.up,distance + angleOffset);
+        hits[2] = Physics2D.Raycast((Vector2)transform.position - offset, -1 * transform.up, distance + angleOffset);
+        hits[1] = Physics2D.Raycast((Vector2)transform.position, -1 * transform.up, distance + angleOffset);
         bool r1 = (hits[0].collider != null) && (hits[0].collider.CompareTag("Lab")),
             r2 = (hits[1].collider != null )&& (hits[1].collider.CompareTag("Lab")),
             r3 = (hits[2].collider != null) && hits[2].collider.CompareTag("Lab");
@@ -39,7 +53,7 @@ public class LabMov : MonoBehaviour
         if (r1) AverageNormal += (Vector3)hits[0].normal;
         if (r2) AverageNormal += (Vector3)hits[1].normal * 2;
         if (r3) AverageNormal += (Vector3)hits[2].normal;
-        rigidbody2D.velocity = Vector3.Cross(AverageNormal.normalized, Vector3.back * -1f) * Speed;
+        rigidbody2D.velocity = Vector3.Cross(AverageNormal.normalized, Vector3.back * -1f) * m_RealSpeed;
 
         float LerpConstant = 0.2f;
         if(HasLeave && LeaveTime >= 0.2f)
@@ -52,15 +66,17 @@ public class LabMov : MonoBehaviour
             transform.right =Vector3.Lerp(transform.right, Vector3.Cross(hits[1].normal, Vector3.back * -1f), LerpConstant);
         else if(r1 && r3)
             transform.right = Vector3.Lerp(transform.right, Vector3.Cross((hits[0].normal + hits[2].normal).normalized, Vector3.back * -1f), LerpConstant);
+
     }
 
 #if UNITY_EDITOR
     protected void OnDrawGizmosSelected()
     {
+        float angleOffset = Mathf.Abs(Mathf.Sin(transform.rotation.z)) * Width;
         for (int i = 0; i < 3; i++)
         {
             UnityEditor.Handles.DrawLine(transform.position + new Vector3((i - 1) * offset.x, 0, 0),
-            transform.position + new Vector3((i - 1) * offset.x, -distance, 0));
+            transform.position + new Vector3((i - 1) * offset.x, -(distance + +angleOffset), 0));
         }
     }
 #endif
