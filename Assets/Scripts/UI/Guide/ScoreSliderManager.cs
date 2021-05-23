5de27b7a4fda1258b683fbfce2afe5d4ea5f4a49
Coldle <1914999099@qq.com>
2021-05-23 21:22:04 +0800

    计分条动画v0.1

--- Assets/Scripts/UI/Guide/ScoreSliderManager.cs
+++ Assets/Scripts/UI/Guide/ScoreSliderManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-interface IScoreGiver 
+interface IScoreGiver
 {
     float GetScore();
     string GetTag();
@@ -11,8 +11,9 @@ interface IScoreGiver
 public class ScoreSliderManager : MonoBehaviour
 {
     public float slider;
-    [SerializeField]
-    private float WholeScore = 0f;
+    [SerializeField] private float WholeScore = 0f;
+    [SerializeField] private GameObject star;
+    [SerializeField] private GameObject starWithScore;
     private ScoreManager scoreManager;
     private List<string> tags;
     private void Awake()
@@ -41,6 +42,25 @@ public class ScoreSliderManager : MonoBehaviour
     {
         slider = scoreManager.GetScore() / WholeScore;
         Mathf.Clamp(slider, 0, 1);
-        this.GetComponent<Slider>().value = slider;
+        StarAnim();
+        this.GetComponent<Slider>().value += (slider - this.GetComponent<Slider>().value) / 20;//缓动
+        Mathf.Clamp(this.GetComponent<Slider>().value, 0, slider);
+    }
+
+    //星星动画
+    private void StarAnim()
+    {
+        if (slider - this.GetComponent<Slider>().value > 0.005)
+        {
+            star.SetActive(false);
+            starWithScore.SetActive(true);
+        }
+        else
+        {
+            star.SetActive(true);
+            starWithScore.SetActive(false);
+            //TODO
+            //旋转,翻转等
+        }
     }
 }
