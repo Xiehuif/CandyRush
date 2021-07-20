7acea80146481d614408cbc1e87278ecd71cc184
Xiehuif <72752074+Xiehuif@users.noreply.github.com>
2021-07-20 16:50:43 +0800

    基本上完成重制，剩下一些地方需要微调

--- Assets/Scripts/Effects/ZoomOut.cs
+++ Assets/Scripts/Effects/ZoomOut.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ZoomOut : MonoBehaviour
 {
     public float cameraZoomEndPoint = 10.0f;//摄像机拉远终点
-
+    bool inZoomOut = false;
 
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -24,23 +24,29 @@ public class ZoomOut : MonoBehaviour
     }
     private IEnumerator ToBegin()
     {
+        while (inZoomOut) yield return 0;
+        inZoomOut = true;
         //摄像机拉近
         for (float schedule = 0; schedule <= 1; schedule += 0.7f * Time.deltaTime)
         {
             Camera.main.orthographicSize = 5f - (5f - cameraZoomEndPoint) * schedule;
             yield return 0;
         }
+        inZoomOut = false;
         yield break;
     }
 
     private IEnumerator ToEnd()
     {
+        while (inZoomOut) yield return 0;
+        inZoomOut = true;
         //摄像机归位
         for (float schedule = 0; schedule <= 1; schedule += 0.7f * Time.deltaTime)
         {
             Camera.main.orthographicSize = cameraZoomEndPoint + (5f - cameraZoomEndPoint) * schedule;
             yield return 0;
         }
+        inZoomOut = false;
         yield break;
     }
 
